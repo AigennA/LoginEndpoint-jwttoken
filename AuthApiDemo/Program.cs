fc57bce9ed0b1900ea15a405bba1d4c1dd0059e1
AigennA <aomurm@gmail.com>
2026-03-12 22:43:45 +0100

    Lägg till Swagger UI med JWT-stöd (Bonus 1)
    Swagger/Swashbuckle konfigurerat med AddSecurityDefinition och
    AddSecurityRequirement så att Authorize-knappen syns i Swagger UI.

--- AuthApiDemo/Program.cs
+++ AuthApiDemo/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using AuthApiDemo.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,35 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
+
+// Swagger med JWT-stöd (Bonus 1)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthApiDemo", Version = "v1" });
+
+    // Lägg till "Authorize"-knapp i Swagger UI
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Klistra in din access token här (utan 'Bearer ' prefix)"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            []
+        }
+    });
+});
 
 // Hämta JWT-nyckel från konfigurationen
 var jwtKey = builder.Configuration["Jwt:Key"]
@@ -35,6 +65,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // OBS! UseAuthentication() MÅSTE komma FÖRE UseAuthorization() i pipeline
 app.UseAuthentication();
